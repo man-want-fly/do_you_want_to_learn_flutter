43d886c696508dfd272702bc4ccdb4471255197b
man.want.fly <man.want.fly@gmail.com>
2021-08-25 17:57:12 +0800

    perf(animation): update hero animation with a fixed hero to make code more simple

--- lib/animation/hero/hero_animation_home.dart
+++ lib/animation/hero/hero_animation_home.dart
@@ -8,19 +8,30 @@ class HeroAnimationHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Hero Animation')),
-      body: Hero(
-        tag: 'hero-tag',
-        child: GestureDetector(
-          child: Column(
-            children: [
-              Image.asset('images/watermelon.png', width: 100),
-              Text('Watermelon')
-            ]),
-            onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (_) => HeroAnimationDetail()));
-          },
-        )
+      body: HeroAnimationSomeHero(
+        child: Column(
+          children: [
+            Image.asset('images/watermelon.png', width: 100),
+            Text('Watermelon')
+          ]
+        ),
+        onTap: () => Navigator.push(
+          context, 
+          MaterialPageRoute(builder: (_) => HeroAnimationDetail())
+        ),
       )
     );
+  }
+}
+
+class HeroAnimationSomeHero extends StatelessWidget {
+  const HeroAnimationSomeHero({Key? key, this.onTap, required this.child}) : super(key: key);
+
+  final VoidCallback? onTap;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(tag: this.toString(), child: GestureDetector(child: child, onTap: onTap));
   }
 }
